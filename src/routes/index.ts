05a0b90f5280ae27ab9f3c9a1e9ebfa3aeb795d9
Jeremy Hull <jchull@gmail.com>
2019-09-07 18:37:12 -0600

    remove default route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,15 +3,5 @@ import userRoutes from './user';
 import adminRoutes from './admin';
 import vehicleRoutes from './vehicle';
 
-import {Request, Response} from 'express';
-
-const home = {
-  path: '/',
-  method: 'get',
-  handler: async (req: Request, res: Response) => {
-    res.send('You have landed at the API root, there are no services here.');
-  }
-};
-
-export default [home, ...productRoutes, ...userRoutes, ...adminRoutes, ...vehicleRoutes];
+export default [...productRoutes, ...userRoutes, ...adminRoutes, ...vehicleRoutes];
 
