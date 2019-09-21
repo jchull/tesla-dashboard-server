f1c5146a6b7e69191a7c4b6b4cef541192d77820
Jeremy Hull <jchull@gmail.com>
2019-09-21 06:44:31 -0600

    additional fields for polling preferences

--- src/model/SyncPreferences.ts
+++ src/model/SyncPreferences.ts
@@ -4,12 +4,17 @@ import {ISyncPreferences} from 'tesla-dashboard-api';
 
 const SyncPreferencesSchema: Schema = new Schema({
   enabled: {type: Boolean},
-  driving_pollingIntervalsSeconds: {type: Number},
+  account_id: {type: String},
+  sleepTriggerSeconds: {type: Number},
+  driving_pollingIntervalSeconds: {type: Number},
+  driving_maxGapSeconds: {type: Number},
   driving_minDurationMinutes: {type: Number},
   charging_minDurationMinutes: {type: Number},
   charging_costPerKwhHome: {type: Number},
   charging_costPerKwhSupercharging: {type: Number},
-  charging_pollingIntervalsSeconds: {type: [Number, Number, Number]}
+  charging_pollingIntervalsSeconds: {type: [Number, Number, Number]},
+  charging_maxGapSeconds: {type: Number},
+
 });
 
 export const SyncPreferences = model('SyncPreferences', SyncPreferencesSchema);
