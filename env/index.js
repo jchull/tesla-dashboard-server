c6c91ade41d0598d87a9e855c490f99424a3a211
Jeremy Hull <jchull@gmail.com>
2019-09-25 20:27:34 -0600

    fix path

--- env/index.js
+++ env/index.js
@@ -1,9 +1,10 @@
 let dotenv = require('dotenv');
+const path = require('path');
 
 // Set default to "development"
 const nodeEnv = process.env.ENV_FILE || 'development';
 const result2 = dotenv.config({
-    path: `./env/${nodeEnv}.env`,
+    path: path.resolve(__dirname,`${nodeEnv}.env`),
 });
 
 if (result2.error) {
